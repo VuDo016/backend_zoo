32580dd898ede76b4a10774c9dcedd7c58cd3b67
VuDo016 <dophuvu016@gmail.com>
2023-05-05 10:49:12 +0700

    config info connect DB mysql deploy

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-    host: 'sql308.byethost7.com',
-    user: 'b7_34025169',
-    password: 'Daophat016',
-    database: 'b7_34025169_zoo',
+    host: 'bs2e8gzevxcbmd8xxy90-mysql.services.clever-cloud.com',
+    user: 'umjivnv0lwjhdhyn',
+    password: 'OlBUcG4eAMPsroGnHTXM',
+    database: 'bs2e8gzevxcbmd8xxy90',
     connectionLimit: 10
   };
